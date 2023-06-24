7cae77bd5b2101b660a23840b424705f6db19cda
rahul deshmukh <rahuldeshmukh4545@gmail.com>
2023-06-24 17:49:32 +0530

    created nested route

---
+++ pages/news/something-important.js
@@ -0,0 +1,8 @@
+//ourdomain.com/news/something-important
+import React from "react";
+
+function SomethingImportant() {
+  return <div>something-important</div>;
+}
+
+export default SomethingImportant;
