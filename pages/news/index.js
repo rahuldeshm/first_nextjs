152494e7b48386c5679accdb96d3f0e5820b18d6
rahul deshmukh <rahuldeshmukh4545@gmail.com>
2023-06-24 18:13:05 +0530

    navigating through the single page application using Link of nextJs

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,11 +1,23 @@
 // ourdomain.com/news
 import React from "react";
+import Link from "next/link";
 
 function NewsPage() {
   return (
-    <div>
+    <>
       <h1>News page</h1>
-    </div>
+      <ul>
+        <li>
+          <Link href="/news/nextjs_is_a_great_fragment">
+            click to see article
+          </Link>
+          NextJS is a great article
+        </li>
+      </ul>
+      <ul>
+        <li>something else</li>
+      </ul>
+    </>
   );
 }
 
